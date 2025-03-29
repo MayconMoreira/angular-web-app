a6bce660ff5c927cf63dc8ee52a9b6966c986083
Maycon Moreira <moreira.maycon98@gmail.com>
2025-03-29 13:50:14 -0300

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyBVWLE04MAxlrOsImso4YPR8zMRWy8NLfI",
+        authDomain: "desenvolvimento-full-stack.firebaseapp.com",
+        projectId: "desenvolvimento-full-stack",
+        storageBucket: "desenvolvimento-full-stack.firebasestorage.app",
+        messagingSenderId: "616071071241",
+        appId: "1:616071071241:web:502d722c187545b4a35ad8"
+      }
+};
